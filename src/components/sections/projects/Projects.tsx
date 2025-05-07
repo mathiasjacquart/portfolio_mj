8c39ce3e101e39434ef6140bd23adb44e9924bf8
Mathias JACQUART <150044178+mathiasjacquart@users.noreply.github.com>
2025-05-07 16:30:51 +0200

    Met à jour le composant Projects en supprimant l'image commentée, ce qui améliore la gestion des ressources et la clarté du code.

--- src/components/sections/projects/Projects.tsx
+++ src/components/sections/projects/Projects.tsx
@@ -25,7 +25,7 @@ export default function Projects() {
         "Gestion panier & paiement : Intégration Stripe, commandes sécurisées",
         "Authentification : Connexion via JWT, rôles utilisateur (admin/client)",
       ],
-      // image: "/images/shopcart.jpg",
+      image: "",
       link: "https://shopcart-omega.vercel.app/",
     },
     {
