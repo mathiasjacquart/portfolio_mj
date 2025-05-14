972ce18b21349ee8fe57372b2de15cc280aeaa7f
Mathias JACQUART <150044178+mathiasjacquart@users.noreply.github.com>
2025-05-14 23:02:40 +0200

    copyright

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,9 +6,23 @@ export default function Footer() {
   return (
     <footer className="mt-16 border-t border-slate-800/40 py-8">
       <div className="flex flex-col items-center justify-between gap-4 text-sm text-slate-400 sm:flex-row">
-        <p className="font-light tracking-wide">
-          © {new Date().getFullYear()} Mathias Jacquart. Tous droits réservés.
-        </p>
+        <div className="flex  gap-4">
+          <p className="font-light tracking-wide">
+            © {new Date().getFullYear()} Mathias Jacquart. Tous droits réservés.{" "}
+            <br></br>
+            <span className="font-light tracking-wide">
+              Design inspiré de{" "}
+              <a
+                href="https://github.com/bchiang7"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="font-bold hover:text-slate-200 transition-colors"
+              >
+                Brittany Chiang
+              </a>
+            </span>
+          </p>
+        </div>
         <Link
           href="/mentions-legales"
           className="font-light tracking-wide hover:text-slate-200 transition-colors"
